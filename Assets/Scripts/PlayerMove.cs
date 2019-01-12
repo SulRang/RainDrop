35b40a33ef1302dfe2d08741535d312f4708c18d
HyunSooJang0 <hunsoo911@gmail.com>
2019-01-12 12:16:48 +0900

    tag : Make_MovePlayer (by Keyboard)

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] [Range(0f, 10f)] float speed = 1;
+    [SerializeField] [Range(0f, 10f)] private float radius = 1;
+
+    private float runningTime = 0;
+    private Vector3 newPos = new Vector3();
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            runningTime += Time.deltaTime * speed;
+            float x = radius * Mathf.Cos(runningTime);
+            float y = radius * Mathf.Sin(runningTime);
+            newPos = new Vector3(x, y, 0f);
+            this.transform.position = newPos;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            runningTime -= Time.deltaTime * speed;
+            float x = radius * Mathf.Cos(runningTime);
+            float y = radius * Mathf.Sin(runningTime);
+            newPos = new Vector3(x, y, 0f);
+            this.transform.position = newPos;
+        }
+        else
+        {
+            this.transform.position = newPos;
+        }
     }
 }
