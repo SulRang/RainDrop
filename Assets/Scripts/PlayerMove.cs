90a10e5da62121409cedccb4f9522f8b742ceca3
HyunSooJang0 <hunsoo911@gmail.com>
2019-01-14 21:21:40 +0900

    tag : MakeScore

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,18 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] [Range(0f, 10f)] float speed = 3;
+    [SerializeField] [Range(0f, 10f)] float radius = 3;
+
+    public static float Score;
+    private float runningTime = 0;
+    private Vector3 newPos = new Vector3();
+
+
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if(col.gameObject.name == "Arrow(Clone)")
+            SceneManager.LoadScene("ResultScene");
     }
 
-    // Update is called once per frame
+    void Awake()
+    {
+        runningTime = 0;
+        Score = 0;
+    }
+
     void Update()
     {
-        
+        // 나중에 BMainActive 없에고 다른걸로 바꾸기
+
+        if(!StartButton.BMainActive)
+            Score += Time.deltaTime;
+
+        if(StartButton.BMainActive)
+        {
+            runningTime += Time.deltaTime * speed;
+            float x = radius * Mathf.Cos(runningTime);
+            float y = radius * Mathf.Sin(runningTime);
+            newPos = new Vector3(x, y, 0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) == true)
+        {
+            runningTime += Time.deltaTime * speed;
+            float x = radius * Mathf.Cos(runningTime);
+            float y = radius * Mathf.Sin(runningTime);
+            newPos = new Vector3(x, y, 0f);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow) == true)
+        {
+            runningTime -= Time.deltaTime * speed;
+            float x = radius * Mathf.Cos(runningTime);
+            float y = radius * Mathf.Sin(runningTime);
+            newPos = new Vector3(x, y, 0f);
+            
+        }
+        this.transform.position = newPos;
     }
 }
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public Text scoreLabel;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        scoreLabel = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!(StartButton.BMainActive))
+            scoreLabel.text = "Score : " + PlayerMove.Score.ToString();
+    }
+}
