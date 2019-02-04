c101ba864e0af989c3a635f69418c62cac26eb28
sulrang <angelace1007@gmail.com>
2019-02-04 20:45:21 +0900

    Make audiomanager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Audio players components.
+    public AudioSource EffectsSource;
+    public AudioSource MusicSource;
+
+    // Random pitch adjustment range.
+    public float LowPitch = 0.95f;
+    public float HighPitch= 1.05f;
+
+    public static AudioManager Instance = null;
+    
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Play a single clip through the music source.
+    public void PlayMusic(AudioClip clip)
+    {
+        MusicSource.clip = clip;
+        MusicSource.Play();
+    }
+
+    // Play a random clip from an array, and randomize the pitch slightly.
+    public void RandomSoundEffect(params AudioClip[] clips)
+    {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(LowPitch, HighPitch);
+
+        EffectsSource.pitch = randomPitch;
+        EffectsSource.clip = clips[randomIndex];
+        EffectsSource.Play();
+    }
+
+}
