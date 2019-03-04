3ce6f057a610420339480ea365620e5e497331ae
HyunSooJang0 <hunsoo911@gmail.com>
2019-03-04 20:45:36 +0900

    tag : fix error

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,49 +4,47 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    // Audio players components.
+    public AudioSource EffectsSource;
+    public AudioSource MusicSource;
+    public AudioSource MusicSource2;
 
-    private static AudioManager _instance = null;
+    // Random pitch adjustment range.
+    public float LowPitch = 0.95f;
+    public float HighPitch = 1.05f;
 
-    public static AudioManager Instance
+    public static AudioManager Instance = null;
+
+    private void Awake()
     {
-        get
+        if (Instance == null)
         {
-            if(_instance == null)
-            {
-                _instance = FindObjectOfType<AudioManager>();
-                if (_instance == null)
-                    Debug.Log("AudioManager is nowhere");
-            }
-            return _instance;
+            Instance = this;
         }
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 
-    private AudioSource MusicSource;
-    private AudioSource EffectSource;
-
-    private float LowPitch = 0.95f;
-    private float HighPitch = 1.00f;
-
-    public void PlayerMusic(AudioClip clip)
+    // Play a single clip through the music source.
+    public void PlayMusic(AudioClip clip)
     {
         MusicSource.clip = clip;
         MusicSource.Play();
     }
 
+    // Play a random clip from an array, and randomize the pitch slightly.
     public void RandomSoundEffect(params AudioClip[] clips)
     {
         int randomIndex = Random.Range(0, clips.Length);
         float randomPitch = Random.Range(LowPitch, HighPitch);
 
-        EffectSource.pitch = randomPitch;
-        EffectSource.clip = clips[randomIndex];
-        EffectSource.Play();
+        EffectsSource.pitch = randomPitch;
+        EffectsSource.clip = clips[randomIndex];
+        EffectsSource.Play();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-    
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using DG;
 using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
