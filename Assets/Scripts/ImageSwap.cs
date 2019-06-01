dbfec3c27b7c347b95a990aafb724892f06dd1a0
HyunSooJang0 <hunsoo911@gmail.com>
2019-06-01 14:45:18 +0900

    tag : fix "dont match button image to soundstatus"

--- Assets/Scripts/ImageSwap.cs
+++ Assets/Scripts/ImageSwap.cs
@@ -5,13 +5,24 @@ using UnityEngine.UI;
 
 public class ImageSwap : MonoBehaviour
 {
-    public Sprite[] Source;
-    public Image image;
-    private bool swap = false;
-
-    public void SwapImage()
+    public enum TYPE
     {
-        if (swap) { image.sprite = Source[0]; swap = !swap; }
-        else { image.sprite = Source[1]; swap = !swap; }
+        SFX,
+        BGM
+    }
+    public Sprite[] Source;     // 0 = off, 1 = on;
+    public Image image;
+    public TYPE buttonType;
+
+    private void Update()
+    {
+        if (AudioManager.Instance.EffectsSource.mute && buttonType == TYPE.SFX)
+            image.sprite = Source[1];
+        else if (!AudioManager.Instance.EffectsSource.mute && buttonType == TYPE.SFX)
+            image.sprite = Source[0];
+        if (AudioManager.Instance.MusicSource[0].mute && buttonType == TYPE.BGM)
+            image.sprite = Source[1];
+        else if (!AudioManager.Instance.MusicSource[0].mute && buttonType == TYPE.BGM)
+            image.sprite = Source[0];
     }
 }
